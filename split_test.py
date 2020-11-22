92e90feaf436887f6f4e860abeaa867ed8b56dd6
Kenneth <kenneth.vangysegem@gmail.com>
2020-11-22 13:13:18 +0100

    first commit

---
+++ split_test.py
@@ -0,0 +1,9 @@
+filename = "./dataset/AdriaaD.jpg"
+
+print(filename.split())
+print(filename.split("/"))
+
+x= filename.split('/')
+print(x[2])
+splitsplitsplit = x[2].split(".")
+print(splitsplitsplit[0])
